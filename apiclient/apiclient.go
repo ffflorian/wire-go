4b5bbd28308429439fef8f8d4379360c1b14be0f
Florian Imdahl <git@ffflorian.de>
2021-03-07 15:29:37 +0100

    fix: Login

--- apiclient/apiclient.go
+++ apiclient/apiclient.go
@@ -18,11 +18,13 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package apiclient
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 )
@@ -35,9 +37,20 @@ type APIClient struct {
 	Timeout  int
 }
 
+type LoginData struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 const (
 	CLIENTS = "clients"
-	LOGIN   = "clients"
+	LOGIN   = "login"
+)
+
+const (
+	GET    = "GET"
+	POST   = "POST"
+	DELETE = "DELETE"
 )
 
 // New returns a new instance of APIClient
@@ -53,9 +66,8 @@ func New(backend string, email string, password string, timeout int) *APIClient 
 // DeleteClient deletes a client of a user
 func (apiClient *APIClient) DeleteClient(clientID string) error {
 	urlPath := apiClient.buildURL(CLIENTS)
-	request := http.Request{Method: "DELETE", URL: urlPath}
 
-	_, requestError := apiClient.request(&request)
+	_, requestError := apiClient.request(DELETE, urlPath, nil)
 	if requestError != nil {
 		return requestError
 	}
@@ -66,9 +78,8 @@ func (apiClient *APIClient) DeleteClient(clientID string) error {
 // GetClients gets all clients of a user
 func (apiClient *APIClient) GetClients(clientID string) (*[]byte, error) {
 	urlPath := apiClient.buildURL(CLIENTS, clientID)
-	request := http.Request{Method: "GET", URL: urlPath}
 
-	clients, requestError := apiClient.request(&request)
+	clients, requestError := apiClient.request(GET, urlPath, nil)
 	if requestError != nil {
 		return nil, requestError
 	}
@@ -79,9 +90,14 @@ func (apiClient *APIClient) GetClients(clientID string) (*[]byte, error) {
 // Login logs the user in
 func (apiClient *APIClient) Login(permanent bool) (*[]byte, error) {
 	urlPath := apiClient.buildURL(LOGIN)
-	request := http.Request{Method: "POST", URL: urlPath}
+	loginData := &LoginData{
+		Email:    apiClient.Email,
+		Password: apiClient.Password,
+	}
+	payloadBuf := new(bytes.Buffer)
+	json.NewEncoder(payloadBuf).Encode(loginData)
 
-	data, requestError := apiClient.request(&request)
+	data, requestError := apiClient.request(POST, urlPath, payloadBuf)
 	if requestError != nil {
 		return nil, requestError
 	}
@@ -91,20 +107,22 @@ func (apiClient *APIClient) Login(permanent bool) (*[]byte, error) {
 	return data, nil
 }
 
-func (apiClient *APIClient) buildURL(fragments ...string) *url.URL {
+func (apiClient *APIClient) buildURL(fragments ...string) string {
 	path := strings.Join(fragments, "/")
-	return &url.URL{Scheme: "https", Host: apiClient.Backend, Path: path}
+	return fmt.Sprintf("https://%s/%s", apiClient.Backend, path)
 }
 
-func (apiClient *APIClient) request(config *http.Request) (*[]byte, error) {
+func (apiClient *APIClient) request(method string, urlPath string, payload io.Reader) (*[]byte, error) {
 	timeout := time.Duration(apiClient.Timeout) * time.Millisecond
-	httpClient := &http.Client{Timeout: timeout}
+	request, _ := http.NewRequest(method, urlPath, payload)
+	request.Header.Set("Content-Type", "application/json")
 
-	fmt.Printf("Sending GET request to \"%s\" with timeout \"%s\" ...\n", config.URL.String(), timeout)
+	client := &http.Client{Timeout: timeout}
+	fmt.Printf("Sending %s request to \"%s\" with timeout \"%s\" ...\n", request.Method, urlPath, timeout)
 
-	response, responseError := httpClient.Do(config)
-	if responseError != nil {
-		return nil, responseError
+	response, requestError := client.Do(request)
+	if requestError != nil {
+		return nil, requestError
 	}
 
 	defer response.Body.Close()
